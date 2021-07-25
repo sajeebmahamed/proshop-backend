701689dde14ed7fb20857828ac2907b37c9e2a9d
sajeeb-staffasia <sajeebahamed@staffasia.org>
2021-07-25 12:15:02 +0600

    product controller added

---
+++ controllers/productController.js
@@ -0,0 +1,32 @@
+// external imports
+const asyncHandler = require('express-async-handler')
+
+// internal imports 
+const Product = require('../models/productModel')
+
+/**
+ * @desc Fetch all Products
+ * @route GET /api/products
+ * @access Public
+ */
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+/**
+ * @desc Fetch a Product
+ * @route GET /api/products/:id
+ * @access Public
+ */
+const getProductById =  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if(product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+module.exports = { getProducts, getProductById }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,36 +1,15 @@
-// external imports
+// external imports 
 const express = require('express')
-const asyncHandler = require('express-async-handler')
-
-// internal imports 
-const Product = require('../models/productModel')
+// internal imports
+const { getProducts, getProductById } = require('../controllers/productController')
 
 // router instance 
 const router = express.Router()
 
-/**
- * @desc Fetch all Products
- * @route GET /api/products
- * @access Public
- */
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
 
-/**
- * @desc Fetch a Product
- * @route GET /api/products/:id
- * @access Public
- */
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
+router.get('/', getProducts)
+
+
+router.get('/:id', getProductById)
 
 module.exports = router
