6522ff3d11d967766b8a2584af08f15e7b2bb1f0
sajeeb-staffasia <sajeebahamed@staffasia.org>
2021-07-25 16:35:05 +0600

    update profile done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,22 @@
-// external imports 
-const express = require('express')
+// external imports
+const express = require("express");
 // internal imports
-const { authUser, getUserProfile, registerUser } = require('../controllers/userController')
-const { protect } = require('../middleware/authMiddleware')
+const {
+   authUser,
+   getUserProfile,
+   registerUser,
+   updateUserProfile,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
-// router instance 
-const router = express.Router()
+// router instance
+const router = express.Router();
 
-router.route('/').post(registerUser)
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router
+   .route("/profile")
+   .get(protect, getUserProfile)
+   .put(protect, updateUserProfile);
 
-
-module.exports = router
+module.exports = router;
