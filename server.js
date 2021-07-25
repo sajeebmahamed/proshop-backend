7b0f4842a1c411e9120cdd6fa98cf82fc2698f87
sajeeb-staffasia <sajeebahamed@staffasia.org>
2021-07-25 09:40:22 +0600

    fixed import, added db config

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // external imports 
 const express = require('express')
-const port = 3000
+const dotenv = require('dotenv')
+const colors = require('colors')
+
+
+// internal imports
+const connectDB = require('./config/db')
 
 // init app
+dotenv.config()
+
+// db connect
+connectDB()
+
 const app = express()
 
 app.get("/", (req, res) => {
     res.send('Hello')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || 6000, () => {
+  console.log(`Example app listening at ${process.env.PORT}`)
 })
