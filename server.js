d763a412708e1104e1ed03d7cef4547a2e0d1163
sajeeb-staffasia <sajeebahamed@staffasia.org>
2021-07-25 11:37:01 +0600

    added error middleware

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const colors = require('colors')
 // internal imports
 const connectDB = require('./config/db')
 const productRoutes = require('./routes/productRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 // init app
 dotenv.config()
@@ -25,6 +26,12 @@ app.use('/api/products', productRoutes)
 app.get("/", (req, res) => {
     res.send('Hello')
 })
+
+// 404 not found handler
+app.use(notFound)
+
+// common error handler
+app.use(errorHandler)
 
 app.listen(process.env.PORT || 6000, () => {
   console.log(`Example app listening at ${process.env.PORT}`)
